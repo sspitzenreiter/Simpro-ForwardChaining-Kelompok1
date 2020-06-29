a023618ef97ff9043ee3149d751e921bf0779863
sspitzenreiter <luthfimnabil17@gmail.com>
2020-06-29 19:04:09 +0700

    Simpro Vue Add

--- ForwardJS/routes/index.js
+++ ForwardJS/routes/index.js
@@ -7,8 +7,8 @@ var RuleEngine = require('node-rules');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-var max_bimbingan = 12;
-var bimbingan_minimal = 0.75;
+// var max_bimbingan = 12;
+
 var rule_nilai_bimbingan_rata = [{
   "name":"nilai_bimbingan",
   "condition":function(R){
@@ -21,7 +21,7 @@ var rule_nilai_bimbingan_rata = [{
 },{
   "name":"nilai_bimbingan",
   "condition":function(R){
-    R.when(this.nilai_bimbingan > 60 && this.nilai_bimbingan <= 85);
+    R.when(this.nilai_bimbingan > 70 && this.nilai_bimbingan <= 85);
   },
   "consequence":function(R){
     this.stat_bimbingan=3;
@@ -30,7 +30,7 @@ var rule_nilai_bimbingan_rata = [{
 },{
   "name":"nilai_bimbingan",
   "condition":function(R){
-    R.when(this.nilai_bimbingan > 45 && this.nilai_bimbingan <= 60);
+    R.when(this.nilai_bimbingan > 55 && this.nilai_bimbingan <= 70);
   },
   "consequence":function(R){
     this.stat_bimbingan=2;
@@ -39,7 +39,7 @@ var rule_nilai_bimbingan_rata = [{
 },{
   "name":"nilai_bimbingan",
   "condition":function(R){
-    R.when(this.nilai_bimbingan > 30 && this.nilai_bimbingan <= 45);
+    R.when(this.nilai_bimbingan > 30 && this.nilai_bimbingan <= 55);
   },
   "consequence":function(R){
     this.stat_bimbingan=1;
@@ -67,7 +67,7 @@ var rule_nilai_sidang_rata = [{
 },{
   "name":"nilai_sidang",
   "condition":function(R){
-    R.when(this.nilai_sidang > 60 && this.nilai_sidang <= 85);
+    R.when(this.nilai_sidang > 70 && this.nilai_sidang <= 85);
   },
   "consequence":function(R){
     this.stat_sidang=3;
@@ -76,7 +76,7 @@ var rule_nilai_sidang_rata = [{
 },{
   "name":"nilai_sidang",
   "condition":function(R){
-    R.when(this.nilai_sidang > 55 && this.nilai_sidang <= 60);
+    R.when(this.nilai_sidang > 55 && this.nilai_sidang <=70);
   },
   "consequence":function(R){
     this.stat_sidang=2;
@@ -102,21 +102,20 @@ var rule_nilai_sidang_rata = [{
   }
 }];
 var rule_total_bimbingan_rata = [{
-  "name":"nilai_sidang",
+  "name":"total_bimbingan",
   "condition":function(R){
-    var presentase_total = this.total_bimbingan/max_bimbingan;
-    R.when(presentase_total>bimbingan_minimal && presentase_total<1);
+    var presentase_total = this.total_bimbingan/this.max_bimbingan;
+    R.when(this.total_bimbingan>this.min_bimbingan && presentase_total<1);
   },
   "consequence":function(R){
     this.stat_total_bimbingan=0.5;
     R.next();
   }
 },{
-  "name":"nilai_sidang",
+  "name":"total_bimbingan",
   "condition":function(R){
-    var presentase_total = this.total_bimbingan/max_bimbingan;
-    R.when(presentase_total==1 && this.nilai_bimbingan>=95);
-    R.when(presentase_total==1 && this.nilai_bimbingan>=85 && this.total_revisi<=2)
+    var presentase_total = this.total_bimbingan/this.max_bimbingan;
+    R.when(presentase_total==1);
   },
   "consequence":function(R){
     this.stat_total_bimbingan=1;
@@ -136,7 +135,7 @@ var lulus = [
   },{
     "name":"lulus_bersyarat",
     "condition":function(R){
-      var stat = this.stat_sidang+this.stat_bimbingan;
+      var stat = this.stat_sidang+this.stat_bimbingan+this.stat_total_bimbingan;
       R.when(stat>=6);
     },"consequence":function(R){
       this.status_lulus="0";
@@ -149,10 +148,12 @@ router.get('/checkRule', (req, res)=>{
   var R = new RuleEngine();
   R.register(rules);
   var fact = {
-    "nilai_bimbingan":70,
-    "nilai_sidang":70,
+    "nilai_bimbingan":65,
+    "nilai_sidang":75,
     "total_revisi":4,
-    "total_bimbingan":10,
+    "total_bimbingan":12,
+    "max_bimbingan":12,
+    "min_bimbingan":10,
     "keterangan":""
   }
   R.execute(fact, function(data){
